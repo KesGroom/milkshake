330ec17a21a7b575c73c25129e68d4d36d46592f
kevin.sanchez1 <kevin.sanchez@agrodatai.com>
2023-10-01 12:23:40 -0500

    Experience finish

--- js/experience.js
+++ js/experience.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     const dotsCustomer = $('.msk-customer-dots');
-    $('.msk-photo-client-experience').slick({
+    $('.msk-customers-slider').slick({
         dots: true,
-        infinite: true,
+        infinite: false,
         speed: 200,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -11,6 +11,52 @@ $(document).ready(function () {
         appendDots: dotsCustomer,
         centerMode: true,
         variableWidth: true,
-        initialSlide: 0,
+        initialSlide: 1,
     });
+
+    const menuButton = $('#menu-button');
+    const closeMenuButton = $('#close-menu-button');
+    const menu = $("#msk-lateral-menu");
+    const body = $('body');
+    menuButton.click(() => {
+        body[0].classList.add('no-scroll');
+        menu[0].classList.add('open');
+    })
+    closeMenuButton.click(() => {
+        body[0].classList.remove('no-scroll');
+        menu[0].classList.remove('open');
+    })
+
+    const popUpBackdrop = $('.msk-popup');
+    const popUp = $('.msk-card-popup');
+    const openPopupButton = $('.msk-section-experience .form-experience .msk-button');
+    const closePopupButton = $('.msk-card-popup .msk-button');
+
+    openPopupButton.click(() => {
+        popUpBackdrop[0].classList.add('msk-activate');
+        popUpBackdrop[0].classList.remove('msk-disable');
+        popUp[0].classList.add('msk-open');
+        popUp[0].classList.remove('msk-close');
+    })
+
+    closePopupButton.click(() => {
+        popUp[0].classList.replace('msk-open', 'msk-close');
+        popUpBackdrop[0].classList.replace('msk-activate', 'msk-disable');
+    })
+
+
+    const stars = $('.msk-stars .fas.fa-star');
+    stars.map((index, star) => {
+        star.addEventListener('click', () => {
+            console.log(`Click en la estrella ${index}`);
+            stars.map((i, star) => {
+                if (i > index) {
+                    star.classList.remove('-color-yellow-dark');
+                } else {
+                    star.classList.add('-color-yellow-dark');
+                }
+            })
+
+        })
+    })
 })
