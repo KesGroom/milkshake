e45d083b5058c580279164696cb0a0f43a5f6263
kevin.sanchez1 <kevin.sanchez@agrodatai.com>
2023-10-01 02:03:54 -0500

    Home Finish

--- js/index.js
+++ js/index.js
@@ -24,6 +24,17 @@ $(document).ready(function () {
         centerMode: true,
         variableWidth: true,
         initialSlide: 1,
-        autoplay: true
     });
+    const menuButton = $('#menu-button');
+    const closeMenuButton = $('#close-menu-button');
+    const menu = $("#msk-lateral-menu");
+    const body = $('body');
+    menuButton.click(() => {
+        body[0].classList.add('no-scroll');
+        menu[0].classList.add('open');
+    })
+    closeMenuButton.click(() => {
+        body[0].classList.remove('no-scroll');
+        menu[0].classList.remove('open');
+    })
 })
